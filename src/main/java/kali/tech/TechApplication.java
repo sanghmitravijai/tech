9be8ebec44ebefe51ea8165996e286126682e738
sanghmitra <sanghmitravijai@gmail.com>
2018-09-23 15:45:12 +0530

    first commit

---
+++ src/main/java/kali/tech/TechApplication.java
@@ -0,0 +1,12 @@
+package kali.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechApplication.class, args);
+	}
+}
