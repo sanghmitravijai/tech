5ec4688f0b251aa46ad8f1c44ed5c2a360130a12
shanirajkali <shanirajkali@gmail.com>
2018-09-25 02:20:22 +0530

    fc

---
+++ view/nav.js
@@ -0,0 +1,8 @@
+function openSideBar(){
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("sidebar").style.display = "100%";
+    document.getElementById("navopener").setAttribute("class","hidden-container");
+}
+function closeSideBar(){
+    document.getElementById("sidebar").style.display = "none";
+}
